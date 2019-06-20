2c30a6ab86204876fe7f27d638abbb3081e5cf5f
gexuelin <1287655436@qq.com>
2019-06-20 20:05:45 +0800

    清空默认内容&调整目录结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入 element 组件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 优先查找文件，如果没有这个文件，就会查找这个目录
 import router from './router'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
